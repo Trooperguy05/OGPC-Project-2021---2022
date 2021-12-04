1fdd36c23bd5b537141e35a01055c590503d687e
EatBugz <77813942+EatBugz@users.noreply.github.com>
2021-12-03 21:28:15 -0800

    Targeting System Finished
    YES! YES! YES! YES!

--- OGPC Project 2021 - 2022/Assets/Scripts/Combat Scripts/PlayerActions.cs
+++ OGPC Project 2021 - 2022/Assets/Scripts/Combat Scripts/PlayerActions.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using System;
 
 public class PlayerActions : MonoBehaviour
 {
     // targeting system variable
     private TargetingSystem ts;
 
-    // create a new targeting system
+    // caching
     void Start() {
+        // create a new targeting system
         ts = new TargetingSystem();
     }
 
-    ///   Character: Dorne's Actions   \\\
+    /////   Character: Dorne's Actions   \\\\\
+    /// Action Wrappers \\\
+    public void execute_dorneStrike() {
+        StartCoroutine(ts.waitForClick(dorneStrike));
+    }
+    /// Actions \\\
+    // basic strike action \\
     public void dorneStrike() {
         // wait for target to return
+        GameObject target = ts.target;
+        Debug.Log(target.name);
         // act on target
     }
+
 }
--- OGPC Project 2021 - 2022/Assets/Scripts/Combat Scripts/TargetingSystem.cs
+++ OGPC Project 2021 - 2022/Assets/Scripts/Combat Scripts/TargetingSystem.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TargetingSystem
 {
-    public TargetingSystem() {
+    // the target of the click \\
+    public GameObject target;
 
-    }
+    // empty constructor because it doesn't need anything \\
+    public TargetingSystem() { }
 
+    // finding the gameobject the player clicked on \\
     public GameObject onClick() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
 
         if (hit.collider != null) {
             return hit.collider.gameObject;
         }
         return null;
+    }
+
+    // method that waits for the player to click \\
+    public IEnumerator waitForClick(Action callback) {
+        while (target == null) {
+            if (Input.GetMouseButtonDown(0)) {
+                target = this.onClick();
+                yield return target;
+            }
+            yield return null;
+        }
+        if (callback != null) callback();
+        target = null;
     }
 }
