735b1d8c3ce0c29ec3356cb738759d8ad8cd8989
EatBugz <77813942+EatBugz@users.noreply.github.com>
2022-01-04 13:38:59 -0800

    Dialogue Finished
    Dialogue system hopefully should be working. Ask me if you need help implementing it.

--- OGPC Project 2021 - 2022/Assets/Scripts/UI Scripts/Dialogue/DialogueManager.cs
+++ OGPC Project 2021 - 2022/Assets/Scripts/UI Scripts/Dialogue/DialogueManager.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     // sentence queue of the dialogue \\
     private Queue<string> sentences;
 
+    // dialogue box game object and text fields \\
+    public GameObject dialogueBox;
+    public TextMeshProUGUI dialogueName;
+    public TextMeshProUGUI dialogueText;
+    // its animator
+    private Animator dialogueBoxAnimator;
+
+    // text typing speed
+    public float typeSpeed = 0.1f;
+
     // initialize the queue \\
     void Start() {
         sentences = new Queue<string>();
+
+        // grabbing the dialogue box animator
+        dialogueBoxAnimator = dialogueBox.GetComponent<Animator>();
     }
 
     // method that starts the dialogue \\
     public void StartDialogue(Dialogue dialogue) {
         Debug.Log("Starting conversation with " + dialogue.name);
+        dialogueBoxAnimator.SetBool("dialogueOn", true);
+        PlayerMovement.playerAbleMove = false;
+        dialogueName.text = dialogue.name;
 
         sentences.Clear();
 
@@ -33,12 +51,23 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        StopAllCoroutines();
+        StartCoroutine(typeText(sentence));
+    }
 
+    // method that types the sentence in keyboard-like fashion \\
+    IEnumerator typeText(string sentence) {
+        dialogueText.text = "";
+        for (int i = 0; i < sentence.Length; i++) {
+            dialogueText.text += sentence[i];
+            yield return new WaitForSeconds(typeSpeed);
+        }
     }
 
     // method that ends the dialogue \\
     public void EndDialogue() {
         Debug.Log("End of conversation");
+        dialogueBoxAnimator.SetBool("dialogueOn", false);
+        PlayerMovement.playerAbleMove = true;
     }
 }
--- OGPC Project 2021 - 2022/Assets/Scripts/UI Scripts/Dialogue/DialogueTrigger.cs
+++ OGPC Project 2021 - 2022/Assets/Scripts/UI Scripts/Dialogue/DialogueTrigger.cs
@@ -5,4 +5,8 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        GameObject.Find("Dialogue Manager").GetComponent<DialogueManager>().StartDialogue(dialogue);
+    }
 }
