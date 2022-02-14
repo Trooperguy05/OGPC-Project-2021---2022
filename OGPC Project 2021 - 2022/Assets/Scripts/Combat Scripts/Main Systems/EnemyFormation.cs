b6770f67f6bcd8f4de9d8228c8056d74b939dace
EatBugz <77813942+EatBugz@users.noreply.github.com>
2022-02-14 12:51:54 -0800

    Healthbars done
    Healthbars for both players and enemies are done

--- OGPC Project 2021 - 2022/Assets/Scripts/Combat Scripts/Main Systems/EnemyFormation.cs
+++ OGPC Project 2021 - 2022/Assets/Scripts/Combat Scripts/Main Systems/EnemyFormation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyFormation : MonoBehaviour
 {
     private CombatManager cM;
+    private HealthbarManager hM;
 
     // the 4 different formations the enemies could be in on the field \\
     private Vector2[] fourEnemyPositions = {
@@ -21,8 +22,11 @@ public class EnemyFormation : MonoBehaviour
     void Start() {
         // grabbing the combat manager
         cM = GameObject.Find("Combat Manager").GetComponent<CombatManager>();
+        // getting the healthbar manager
+        hM = GameObject.Find("Healthbar Manager").GetComponent<HealthbarManager>();
     }
 
+    // method that reorganizes the field of enemies \\
     public void organizeField() {
         // checking the amount of enemies
         if (cM.enemiesInCombat.Count == 4) {
@@ -42,6 +46,20 @@ public class EnemyFormation : MonoBehaviour
         }
         if (cM.enemiesInCombat.Count == 1) {
             cM.enemiesInCombat[0].transform.position = oneEnemyPosition;
+        }
+
+        // move the enemies' healthbars
+        if (cM.enemy1 != null) {
+            hM.enemy1Healthbar.GetComponent<RectTransform>().position = new Vector2(cM.enemy1.transform.position.x + 2, cM.enemy1.transform.position.y);
+        }
+        if (cM.enemy2 != null) {
+            hM.enemy2Healthbar.GetComponent<RectTransform>().position = new Vector2(cM.enemy2.transform.position.x + 2, cM.enemy2.transform.position.y);
+        }
+        if (cM.enemy3 != null) {
+            hM.enemy3Healthbar.GetComponent<RectTransform>().position = new Vector2(cM.enemy3.transform.position.x + 2, cM.enemy3.transform.position.y);
+        }
+        if (cM.enemy4 != null) {
+            hM.enemy4Healthbar.GetComponent<RectTransform>().position = new Vector2(cM.enemy4.transform.position.x + 2, cM.enemy4.transform.position.y);
         }
     }
 }
