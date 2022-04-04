7c3ae343438c4996cc69c8c9e63f9a565ded2053
Trooperguy05 <Tyry2005@outlook.com>
2022-04-03 23:50:13 -0700

    Main menu work
    Main menu is looking better.  Currently in NYC and doing work G a m i n g

---
+++ OGPC Project 2021 - 2022/Assets/Scripts/Menu Scripts/MainMenuAudio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAudio : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource AS;
+    public AudioClip regularMenu;
+    public AudioClip kazooMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float percentChance = Random.Range(0,1001);
+        if (percentChance == 1) {
+            AS.PlayOneShot(kazooMenu, 1);
+        }
+        Debug.Log(percentChance);
+    }
+
+}
